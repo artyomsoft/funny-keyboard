4ba9e0ade992c28a0d1cb2aaec6646de371fcc12
artyomsoft <artyomsoft@gmail.com>
2026-02-06 04:40:40 +0200

    Iteration 3: Added virtual keyboard

--- funny-kbd.c
+++ funny-kbd.c
@@ -3,8 +3,29 @@
 #include <linux/notifier.h>
 #include <linux/keyboard.h>
 #include <linux/workqueue.h>
+#include <uapi/linux/input-event-codes.h>
+#include <linux/input.h>
 
 MODULE_LICENSE("GPL");
+
+static struct input_dev *virt_kbd;
+
+unsigned int eng_word[] = {
+    KEY_H,
+    KEY_E,
+    KEY_L,
+    KEY_L,
+    KEY_O};
+
+static void send_keys(struct input_dev *kbd, unsigned int *keys, size_t num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        input_report_key(kbd, keys[i], 1);
+        input_report_key(kbd, keys[i], 0);
+        input_sync(kbd);
+    }
+}
 
 static struct workqueue_struct *kb_wq;
 
@@ -23,6 +44,11 @@ static void kb_work_fn(struct work_struct *work)
         container_of(work, struct kb_work, work);
 
     pr_info("funny-kbd: action = %lu keycode = %u  shift = %d\n", kw->action, kw->keycode, kw->shift);
+
+    if (kw->keycode == KEY_COMMA)
+    {
+        send_keys(virt_kbd, eng_word, sizeof(eng_word) / sizeof(unsigned int));
+    }
 
     kfree(kw);
 }
@@ -56,13 +82,50 @@ static int keyboard_notifier_cb(struct notifier_block *nblock,
 static struct notifier_block kbd_nb = {
     .notifier_call = keyboard_notifier_cb};
 
+static int init_virt_kbd(void)
+{
+    int ret;
+
+    virt_kbd = input_allocate_device();
+
+    if (!virt_kbd)
+    {
+        pr_err("input_allocate_device failed\n");
+        return -ENOMEM;
+    }
+
+    virt_kbd->name = "Funny Keyboard";
+    virt_kbd->phys = "virtual/filtered/kbd";
+    virt_kbd->id.bustype = BUS_VIRTUAL;
+    virt_kbd->id.vendor = 0xDEAD;
+    virt_kbd->id.product = 0xBEEF;
+
+    set_bit(EV_KEY, virt_kbd->evbit);
+    set_bit(EV_REP, virt_kbd->evbit);
+
+    for (int i = 0; i < KEY_CNT; i++)
+    {
+        set_bit(i, virt_kbd->keybit);
+    }
+
+    ret = input_register_device(virt_kbd);
+
+    if (ret)
+    {
+        pr_err(KERN_ERR "input_register_device failed: %d\n", ret);
+        input_free_device(virt_kbd);
+        return ret;
+    }
+
+    return 0;
+}
 static int __init funny_kbd_init(void)
 {
     pr_info("funny-kbd: Module loaded\n");
     kb_wq = alloc_workqueue("kb_wq", WQ_UNBOUND, 1);
     if (!kb_wq)
         return -ENOMEM;
-
+    init_virt_kbd();
     return register_keyboard_notifier(&kbd_nb);
 }
 
@@ -70,6 +133,9 @@ static void __exit funny_kbd_exit(void)
 {
     unregister_keyboard_notifier(&kbd_nb);
 
+    input_unregister_device(virt_kbd);
+    input_free_device(virt_kbd);
+    
     flush_workqueue(kb_wq);
     destroy_workqueue(kb_wq);
 
